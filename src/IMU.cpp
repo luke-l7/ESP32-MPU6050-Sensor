b7e94d1c53eeebe606724e742c3d6d2e28dec0ec
Lucas Lucia <lucas.lucia9@protonmail.com>
2022-08-31 00:48:07 +0300

    Added basic functionality

--- src/IMU.cpp
+++ src/IMU.cpp
@@ -1,1 +1,13 @@
 #include <IMU.hpp>
+
+int IMU::Begin() noexcept
+{
+    if (!mpu_.begin()) 
+    {
+        return 1;
+    }
+    mpu_.setAccelerometerRange(A_RANGE);
+    mpu_.setGyroRange(G_RANGE);
+    mpu_.setFilterBandwidth(B_RANGE);
+    return 0;
+}
--- src/IMU.hpp
+++ src/IMU.hpp
@@ -1,10 +1,20 @@
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+
 class IMU
 {
 private:
-    /* data */
+    Adafruit_MPU6050 mpu_;
+    static constexpr mpu6050_accel_range_t A_RANGE = MPU6050_RANGE_8_G;
+    static constexpr mpu6050_gyro_range_t G_RANGE = MPU6050_RANGE_500_DEG;
+    static constexpr mpu6050_bandwidth_t B_RANGE = MPU6050_BAND_5_HZ;
 public:
-    IMU(/* args */);
-    ~IMU();
+
+
+    IMU(/* args */): mpu_(){};
+    ~IMU(){};
+    int Begin() noexcept;
 };
 
 IMU::IMU(/* args */)
