51c93674525c612504beb369c514e404f8c1e997
Lucas Lucia <lucas.lucia9@protonmail.com>
2022-08-31 01:01:54 +0300

    Added initialization files

---
+++ src/Sensor.h
@@ -0,0 +1,19 @@
+class Sensor
+{
+private:
+    /* data */
+public:
+    Sensor(/* args */);
+    ~Sensor();
+    /*Trasmit bytes given the UART in IMU style*/
+    void Trasmit();
+};
+
+Sensor::Sensor(/* args */)
+{
+}
+
+
+Sensor::~Sensor()
+{
+}
