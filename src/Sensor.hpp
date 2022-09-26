f7acdc256fa08cad0e542b981f1123e6965f66b3
Lucas Lucia <101108230+luke-l7@users.noreply.github.com>
2022-09-26 22:42:44 +0300

    init commit

--- src/Sensor.hpp
+++ src/Sensor.hpp
@@ -1,21 +1,22 @@
 #include <stdio.h>
-
+/**
+ * Sensor Simulator class 
+ */
 class Sensor
 {
 private:
     size_t missed_;
+    const char start_letter_ = '$'; ///> Command starting letter
+    const char end_letter_ = '#'; ///> Command ending letter
+    const char* ping_command_ = "0001"; ///> Sensor ping command
+    const char* ping_response_ = "11"; ///> Sensor ping response
+    const char* init_message_ = "00051"; ///> Sensor initialization message
+    void transmit(char* buff, size_t size);
+    void recieve(char* buff, size_t size);
 public:
-    Sensor(/* args */);
-    ~Sensor();
-    /*Trasmit bytes given the UART in IMU style*/
-    void Trasmit();
+    Sensor(/* args */){};
+    ~Sensor(){};
+    void active();
 };
 
-Sensor::Sensor(/* args */)
-{
-}
 
-
-Sensor::~Sensor()
-{
-}
